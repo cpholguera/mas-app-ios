6be3071f3330482a01b055678b8a245ff3bb9af3
Stefan Bernhardsgrütter <s.bernhardsgruetter@gmail.com>
2025-09-25 09:58:34 +0200

    changed the orders of the tests to match the ones on the android test app: 1. good case, 2. bad case, 3.exception

--- MASTestApp/MastgTest.swift
+++ MASTestApp/MastgTest.swift
@@ -15,11 +15,11 @@ struct MastgTest {
             do {
                 let sensitiveString = "Hello from the OWASP MASTG Test app."
 
-                // case 1: Demo implements a case which fails a test
-                r.add(status: .fail, message: "The app implemented a demo which failed the test.")
-
-                // case 2: Demo implements a case which passes a test
+                // case 1: Demo implements a case which passes a test
                 r.add(status: .pass, message: "The app implemented a demo which passed the test with the following value: '\(sensitiveString)'")
+
+                // case 2: Demo implements a case which fails a test
+                r.add(status: .fail, message: "The app implemented a demo which failed the test.")
 
                 throw NSError(domain: "MastgTest", code: 1, userInfo: [NSLocalizedDescriptionKey: "Something went wrong during the demo."])
             }
